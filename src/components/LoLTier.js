a664b25aadc6ce2ac3ac70bec2b6a00408c7dac8
DESKTOP-ALHPHIE\yms <msg3265@gmail.com>
2024-03-28 15:30:27 +0900

    api 테스트

--- src/components/LoLTier.js
+++ src/components/LoLTier.js
@@ -174,11 +174,14 @@ function LoLTier(props) {
       const riot_proxy = window.location.hostname === 'localhost' ? '' : '/riot_proxy';
       try {
         // 설명* Promise.all() 메서드를 사용하여 비동기 작업을 기다림
-        // const response = await Promise.all(members.map(member => axios.get(`${riot_proxy}/lol/league/v4/entries/by-summoner/${member.summonerId}?api_key=${process.env.REACT_APP_RIOTGAMES_KEY}`)));
+        const responses = await Promise.all(members.map(member => axios.get(`${riot_proxy}/lol/league/v4/entries/by-summoner/${member.summonerId}?api_key=${process.env.REACT_APP_RIOTGAMES_KEY}`)));
         const response = await Promise.all(members.map(member => axios.get(`https://kr.api.riotgames.com/lol/league/v4/entries/by-summoner/${member.summonerId}?api_key=${process.env.REACT_APP_RIOTGAMES_KEY}`)));
-        const res = await Promise.all(members.map(member => axios.get(`${riot_proxy}/lol/platform/v3/champion-rotations?api_key=${process.env.REACT_APP_RIOTGAMES_KEY}`)));
+        const res = await axios.get(`${riot_proxy}/lol/platform/v3/champion-rotations?api_key=${process.env.REACT_APP_RIOTGAMES_KEY}`);
+        const resp = await axios.get(`https://kr.api.riotgames.com/lol/platform/v3/champion-rotations?api_key=${process.env.REACT_APP_RIOTGAMES_KEY}`);
+        console.log(responses);
         console.log(response);
         console.log(res);
+        console.log(resp);
 
         const newData = response.map((res, index) => { 
           const filterTire = res.data.filter(data => data.queueType === 'RANKED_SOLO_5x5');
